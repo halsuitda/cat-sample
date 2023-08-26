41f8c5fd2ef0dba01e26ae52797b6fcc18a3fe10
sussa3007 <sussa1933@gmail.com>
2023-08-26 15:47:04 +0900

    refactor: User Entity Cat, Cart 연관 관계 매핑, 관계 메소드 구현

--- src/main/java/com/study/cat/user/entity/User.java
+++ src/main/java/com/study/cat/user/entity/User.java
@@ -1,9 +1,11 @@
 package com.study.cat.user.entity;
 
 import com.study.cat.audit.AuditTable;
+import com.study.cat.cart.entity.Cart;
 import com.study.cat.cat.entity.Cat;
 import com.study.cat.constant.LoginType;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -15,8 +17,9 @@ import java.util.Set;
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Getter
+@EqualsAndHashCode(callSuper = true)
 public class User extends AuditTable {
 
     @Column(nullable = false, unique = true)
@@ -25,20 +28,33 @@ public class User extends AuditTable {
     @Column(nullable = false, unique = true)
     private String nickName;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private LoginType loginType;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
     @ToString.Exclude
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("id")
     private Set<Cat> cats = new LinkedHashSet<>();
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<String> roles = new ArrayList<>();
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("id")
+    private Set<Cart> carts = new LinkedHashSet<>();
+
+    public void addCat(Cat cat) {
+        this.cats.add(cat);
+    }
+
+    public void addCart(Cart cart) {
+        this.carts.add(cart);
+    }
 
 
 }
