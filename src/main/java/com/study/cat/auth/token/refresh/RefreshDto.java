72393b64e55c91fde01e2df9dafd31cd684b853a
sussa3007 <sussa1933@gmail.com>
2023-08-27 23:16:06 +0900

    feat: RefreshDto 작성

---
+++ src/main/java/com/study/cat/auth/token/refresh/RefreshDto.java
@@ -0,0 +1,31 @@
+package com.study.cat.auth.token.refresh;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshDto {
+
+    private String email;
+
+    private String refreshToken;
+
+    private LocalDateTime createdAt;
+
+    public RefreshDto(RefreshEntity entity) {
+        this.email = entity.getEmail();
+        this.refreshToken = entity.getRefreshToken();
+        this.createdAt = entity.getCreatedAt();
+    }
+
+    public static RefreshDto of(RefreshEntity entity) {
+        return new RefreshDto(entity);
+    }
+}
