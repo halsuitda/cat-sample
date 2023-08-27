080b7cec77c3c377d4cc883e6f7a8e42be0b5c30
sussa3007 <sussa1933@gmail.com>
2023-08-27 23:16:28 +0900

    feat: RefreshRepository 작성

---
+++ src/main/java/com/study/cat/auth/token/refresh/RefreshRepository.java
@@ -0,0 +1,6 @@
+package com.study.cat.auth.token.refresh;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshRepository extends CrudRepository<RefreshEntity, String> {
+}
